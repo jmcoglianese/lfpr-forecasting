fe780f57a59e872a24b374b09a14bea26b5edd41
John Coglianese <coglianese@fas.harvard.edu>
2018-04-06 17:17:50 -0400

    New model run

--- scripts/test_kernels.py
+++ scripts/test_kernels.py
@@ -12,9 +12,9 @@ try:
 except OSError:
     os.chdir('/n/home00/jcoglianese/Dropbox/Documents/Research/LFPR/Forecasting')
 
-logit_link = {'transform': lambda x: logit(x / 100), 
-             'untransform': lambda x: expit(x) * 100} 
-iden_link = {'transform': lambda x: x, 
+logit_link = {'transform': lambda x: logit(x / 100),
+             'untransform': lambda x: expit(x) * 100}
+iden_link = {'transform': lambda x: x,
              'untransform': lambda x: x}
 
 def transform_y(input, link = iden_link, baseline = None):
@@ -23,16 +23,16 @@ def transform_y(input, link = iden_link, baseline = None):
     else:
         return link['transform'](input) - link['transform'](baseline)
 def untransform_y(input, link = iden_link, baseline = None):
-    if baseline is None: 
+    if baseline is None:
         return link['untransform'](input)
     else:
         return link['untransform'](input + link['transform'](baseline))
-    
+
 def normdata(input, mean = None, std = None):
     mean_hat = mean if mean is not None else np.mean(input, axis = 0)
     std_hat = std if std is not None else np.std(input, axis = 0)
     normalized_input = (input - mean_hat) / std_hat
-    
+
     if mean is not None:
         return normalized_input
     else:
@@ -53,12 +53,12 @@ k_lin_ugapl1 = GPy.kern.Linear(input_dim = 1, variances = [0.2 ** 2], active_dim
 k_lin_ugapl2 = GPy.kern.Linear(input_dim = 1, variances = [0.2 ** 2], active_dims = [6])
 
 k1 = k_se_agesex + k_lin_t * k_se_agesex
-k2 = k_se_agesex + k_lin_t * k_se_tagesex
-k3 = k_se_agesex + k_lin_cohort * k_se_cohort * k_se_agesex
-k4 = k_se_agesex + k_lin_t * k_se_tagesex + k_se_cohort
+k2 = k_se_tagesex + k_lin_t * k_se_tagesex
+k3 = k_se_agesex * k_se_cohort + k_lin_cohort * k_se_cohort * k_se_agesex
+k4 = k_se_tagesex + k_lin_t * k_se_tagesex + k_se_cohort
 k5 = k_se_agesex + k_lin_cohort * k_se_cohort
-k6 = k_se_agesex + k_lin_t * k_se_tagesex + k_lin_ugap * k_se_agesex
-k7 = k_se_agesex + k_lin_t * k_se_tagesex + k_lin_ugap * k_se_agesex + k_lin_ugapl1 * k_se_agesex + k_lin_ugapl2 * k_se_agesex
+k6 = k_se_tagesex + k_lin_t * k_se_tagesex + k_lin_ugap * k_se_agesex
+k7 = k_se_tagesex + k_lin_t * k_se_tagesex + k_lin_ugap * k_se_agesex + k_lin_ugapl1 * k_se_agesex + k_lin_ugapl2 * k_se_agesex
 
 kernels = [k1, k2, k3, k4, k5, k6, k7]
 ms = []
@@ -66,35 +66,35 @@ ms = []
 for i, kern in enumerate(kernels):
     proc_data = data.copy()
     train = proc_data[proc_data['year'] < 2000].copy()
-    
+
     X_train = train[['sex', 'age', 'year', 'cohort', 'ugap', 'ugap_lag1', 'ugap_lag2']].copy().values
     y_train = transform_y(train['lfp'].copy().values, logit_link)
     X_all = proc_data[['sex', 'age', 'year', 'cohort', 'ugap', 'ugap_lag1', 'ugap_lag2']].copy().values
-    
+
     XN_train, X_mean, X_std = normdata(X_train)
     XN_all = normdata(X_all, X_mean, X_std)
     yN_train, y_mean, y_std = normdata(y_train)
-    
+
     yN_train = yN_train.reshape(yN_train.shape[0], 1)
-    
+
     m = GPy.models.GPRegression(XN_train, yN_train, kernel = kern)
     m.Gaussian_noise.variance = 0.2 ** 2
     m.optimize()
     print m
     print m['']
     ms.append(deepcopy(m))
-    
+
     pred, var = m.predict(XN_all)
     y_pred = untransform_y(np.squeeze(pred) * y_std + y_mean, logit_link)
     y_se = np.sqrt(var * y_std ** 2)
     data['lfp_pred{}'.format(i+1)] = y_pred
     data['lfp_se{}'.format(i+1)] = y_se
-    
+
     for j, subk in enumerate(m.kern.parameters):
         subpred, subvar = m.predict(XN_all, kern = subk)
         data['lfp_pred{}_{}'.format(i+1, j+1)] = untransform_y(np.squeeze(subpred) * y_std + y_mean, logit_link)
-    
+
 data.to_csv('data/lfpr_gpy_testkernels_pred.csv')
 
-with open('output/test_kernels.pkl', 'wb') as f:
+with open('output/pkl/test_kernels.pkl', 'wb') as f:
     pickle.dump(ms, f)
